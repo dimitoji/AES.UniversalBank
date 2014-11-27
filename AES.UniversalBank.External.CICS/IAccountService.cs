4562f76e1032b101092357914fcdd063051c1bfd
Alejandro Figueroa <alexphi@gmail.com>
2014-11-27 10:19:35 -0500

    Initial services definition

---
+++ AES.UniversalBank.External.CICS/IAccountService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AES.UniversalBank.External.CICS
+{
+    public interface IAccountService
+    {
+        Common.Entities.Account GetAccount(string id);
+        IList<Common.Entities.AccountTransaction> GetAccountTransactions(string id);
+
+        void AddAccountTransaction(string accountId, Common.Entities.AccountTransaction transaction);
+    }
+}
---
+++ AES.UniversalBank.External.Loans/ILoanService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AES.UniversalBank.External.Loans
+{
+    public interface ILoanService
+    {
+        IList<Common.Entities.Loan> GetCustomerLoans(string customerId);
+    }
+}
