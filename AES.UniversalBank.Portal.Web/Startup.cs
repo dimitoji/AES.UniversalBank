555fdff60965bb5696f48844ecd88d43f3d75399
Alejandro Figueroa <alexphi@gmail.com>
2014-11-27 10:46:59 -0500

    Portal and Payments App structure

---
+++ AES.UniversalBank.Portal.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AES.UniversalBank.Portal.Web.Startup))]
+namespace AES.UniversalBank.Portal.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
